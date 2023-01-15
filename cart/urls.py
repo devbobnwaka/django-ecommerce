45c3562f8b7df9753e3bd45ca8f3b9eb6fb76af2
devbobnwaka <nwakachibueze@gmail.com>
2023-01-15 16:15:30 +0100

    increase and reduce cart in sesion

--- cart/urls.py
+++ cart/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('cart/', views.cart, name="cart"),
     path('add_cart/<slug:slug>/', views.add_cart, name="add_cart"),
     path('remove_cart/<slug:slug>/', views.remove_cart, name="remove_cart"),
+    path('increase_cart_quantity/<slug:slug>', views.increase_cart_quantity, name="increase_cart"),
+    path('decrease_cart_quantity/<slug:slug>', views.decrease_cart_quantity, name="decrease_cart"),
 ]
--- cart/views.py
+++ cart/views.py
@@ -3,11 +3,10 @@ from store.models import Product, ProductImage
 
 # Create your views here.
 def cart(request):
-    print(request.session.get('slug'))
+    quantity_dict = request.session.get('quantity')
     products = Product.objects.filter(slug__in=request.session['slug'])
-    context = {
-        'products':products,
-    }
+    pro = {'products':products}
+    context = quantity_dict | pro
     return render(request, 'cart/cart.html', context)
 
 
@@ -16,8 +15,11 @@ def add_cart(request, slug):
     if 'slug' in request.session:
         if slug not in request.session['slug']:
             request.session['slug'].append(slug)
+            request.session['quantity'][slug] = 1
     else:
         request.session['slug'] = [slug]
+        request.session['quantity'][slug] = 1
+        
     # del request.session['slug']
     request.session.modified = True
 
@@ -26,5 +28,27 @@ def add_cart(request, slug):
 
 def remove_cart(request, slug):
     request.session['slug'].remove(slug)
+    del request.session['quantity'][slug]
     request.session.modified = True
     return redirect(request.META.get('HTTP_REFERER'))
+
+
+def increase_cart_quantity(request, slug):
+    request.session['quantity'][slug] += 1
+    print(request.session['quantity'][slug])
+    request.session.modified = True
+    return redirect(request.META.get('HTTP_REFERER'))
+
+
+def decrease_cart_quantity(request, slug):
+    if request.session['quantity'][slug] > 1:
+        request.session['quantity'][slug] -= 1
+        print(request.session['slug'])
+    else:
+        request.session['slug'].remove(slug)
+        del request.session['quantity'][slug]
+        print(request.session['slug'])
+    request.session.modified = True
+    return redirect(request.META.get('HTTP_REFERER'))
+
+
