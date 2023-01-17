f23c42340a294f433d45d61413e27bc84c327268
devbobnwaka <nwakachibueze@gmail.com>
2023-01-17 17:36:19 +0100

    formatted numbers and added the sub_total

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -1,7 +1,18 @@
 
 
-def counter(request):
+def custom_context_processor(request):
+    sub_total = 0
     cart_count = 0
     for val in request.session['quantity'].values():
         cart_count += val
-    return dict(cart_count=cart_count) 
+    for price in request.session['discount_price_total'].values():
+        sub_total += price
+    return dict(cart_count=cart_count, sub_total="{:,}".format(sub_total)) 
+
+
+# def sub_total(request):
+#     sub_total = 0
+#     for price in request.session['discount_price_total'].values():
+#         sub_total += price
+#         print(price)
+#     return dict(sub_total=sub_total)
